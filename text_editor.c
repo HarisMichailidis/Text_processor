dacd9ddd5b9206672774ed6d0d33536d383de625
Charalampos Michailidis <charalampos.michailidis@gmail.com>
2014-01-22 18:09:19 +0200

    more functions added

--- text_editor.c
+++ text_editor.c
@@ -7,15 +7,17 @@ FILE *open_file(char file_name[25]);
 int Search_in_File(FILE *fp, char *str);
 int Find_once(FILE *fp, char *str);
 void Delete_rows(int starting_row, int numb_of_rows);
-void copy_n_replace(
+void replace_n_store(
    char starting_string[30], char ending_string[30],  
    FILE *source_file,FILE *destination_file);
+void replace_with_comma(char input_line[512],FILE *output_file);
+char *str_replace(char *orig, char *rep, char *with);
 
 int main()
 {
    char ch;
    FILE *fs,*fb,*fo;
-   char mark_rep[]="MARKET",new_lots[]="New lots";
+   char mark_rep[]="Market report",new_lots[]="New lots";
 
 
    fs=open_file("test.txt");   //test.txt"); //firmA4_1008-2016.txt
@@ -26,7 +28,7 @@ int main()
    int counter=0;
    int others_begin,others_end;
 
-   copy_n_replace(mark_rep,new_lots,fs,fo);
+   replace_n_store(mark_rep,new_lots,fs,fo);
 
    fclose(fs);
    fclose(fb);
@@ -114,7 +116,7 @@ int Find_once(FILE *fp, char *str) {
    return(0);
 }
 
-void copy_n_replace(
+void replace_n_store(
    char starting_string[30], char ending_string[30], 
    FILE *source_file,FILE *destination_file)
 {
@@ -126,25 +128,81 @@ void copy_n_replace(
 
    Find_once(source_file, starting_string);
    while(fgets(temp, 512, source_file) != NULL) {
-      printf("%s\n",temp);
-  /*    if((strstr(temp, str)) != NULL) {
-         printf("A match found on line: %d\n", line_num);
-         printf("\n%s\n", temp);
-         find_result++;
+      if((strstr(temp, ending_string)) != NULL) {
+         //found the end
+         printf("done with the first part\n");
+         break;
+      }else{ 
+
+         //printf("%s\n",temp);
+         replace_with_comma(temp,destination_file);
       }
-    */
+   
    }
 
+}
+
+
+void replace_with_comma(char input_line[512],FILE *output_file){
+
+   char output[512];
+   char string1[30];
+
+   int i=0;
+   while(input_line[i]!='\n'){
+
+      //code for replacing spaces with commas
 
 
 
 
+      printf("%c",input_line[i]);
+      i++;
+   }
+   //printf("\n");
+}
 
+// You must free the result if result is non-NULL.
+char *str_replace(char *orig, char *rep, char *with) {
+    char *result; // the return string
+    char *ins;    // the next insert point
+    char *tmp;    // varies
+    int len_rep;  // length of rep
+    int len_with; // length of with
+    int len_front; // distance between rep and end of last rep
+    int count;    // number of replacements
 
+    if (!orig)
+        return NULL;
+    if (!rep)
+        rep = "";
+    len_rep = strlen(rep);
+    if (!with)
+        with = "";
+    len_with = strlen(with);
 
-//   int others_end=others_begin+Find_once(source_file, ending_string)-2;
-//   printf("Others.csv will end on line: %d\n",others_end);
+    ins = orig;
+    for (count = 0; tmp = strstr(ins, rep); ++count) {
+        ins = tmp + len_rep;
+    }
 
+    // first time through the loop, all the variable are set correctly
+    // from here on,
+    //    tmp points to the end of the result string
+    //    ins points to the next occurrence of rep in orig
+    //    orig points to the remainder of orig after "end of rep"
+    tmp = result = malloc(strlen(orig) + (len_with - len_rep) * count + 1);
 
+    if (!result)
+        return NULL;
 
+    while (count--) {
+        ins = strstr(orig, rep);
+        len_front = ins - orig;
+        tmp = strncpy(tmp, orig, len_front) + len_front;
+        tmp = strcpy(tmp, with) + len_with;
+        orig += len_front + len_rep; // move to next "end of rep"
+    }
+    strcpy(tmp, orig);
+    return result;
 }
